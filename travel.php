df85f756943c8234dec60c23ec3840d5dde7ae4f
emma2625 <emmanuelodobo10@gmail.com>
2022-06-16 18:12:11 +0100

    Added LightBox Plugin

---
+++ travel.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>TRAVEL</title>
+    <!-- <link rel="stylesheet" href="assets/css/airline.css"> -->
+    <link rel="stylesheet" href="assets/fontawsome/css/all.css">
+    <link rel="stylesheet" href="assets/css/bootstrap5.min.css">
+    <link rel="stylesheet" href="assets/css/travel.css">
+    <link rel="stylesheet" href="assets/css/lightboxed.css">
+</head>
+<body>
+    <section class="headerr">
+    <?php require_once "assets/modules/nav.php"; ?>
+    <h1 class="follow text-white mx-5" style="text-shadow: -10px 5px 9px black;">
+          Follow the sun this summer <br> Save up to 25%
+        </h1>
+         <div class="mx-5 mb-5">
+                        <button type="submit" name="register" class="btn btn-outline-light btn-lg">Book Now</button>
+                    </div>
+     </section>
+
+          <div style="background-color: rgba(151, 150, 150, 0.459);">
+                    <section>
+        <div class="start mx-auto pt-5" style="width: 70%;">
+          <h4>Our latest flight deals</h4> <br>
+          <h6 class="from d-flex gap-5">From <span class="text-purple d-block">F.C.T ABUJA <hr style="margin-top: 0px;"></span></h6>
+          <div class="first-block d-flex" style="gap: 20px;">
+            <div class="firstimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px; border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+              <img src="assets/img/london gatwick.jpg" rel="group1" class="lightboxed" height="520px" width="400px" style="border-top-left-radius: 20px; border-top-right-radius: 20px; ">
+              <h5 style="margin-bottom: 2px;margin-top: 20px;padding-bottom: -20px; padding-left: 20px;">London Gatwwick</h5> <br>
+              <h6 style="margin-left: 20px;"> 11 sep 2022 - 22 sep 2022 <br>
+              Economy from <br>
+              USD 776 <br>
+            </h6>
+            </div>
+            <div class="block">
+              <div class="thirdimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+                <img src="assets/img/london heatrow.jpg" alt="" rel="group1" class="lightboxed" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " height="250px" width="260px">
+                <h5 style="margin-bottom: 10px;margin-top: 20px;padding-bottom: 20px; padding-left: 20px;">London Heathrow</h5>
+                <h6 style="margin-left: 20px;"> 11 sep 2022 - 22 sep 2022 <br>
+                  Economy from <br>
+                  USD 776 <br>
+                </h6>
+              </div> <br>
+              <div class="fourthimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+                <img src="assets/img/newyork.jpg" rel="group1" class="lightboxed" style="border-top-left-radius: 20px; border-top-right-radius: 20px; ">
+                <h5 style="margin-top: 20px;padding-bottom: 30px; padding-left: 20px;">New York</h5>
+                <h6 style="margin-left: 20px;"> 11 sep 2022 - 22 sep 2022 <br>
+                  Economy from <br>
+                  USD 776 <br>
+                </h6>
+              </div>
+            </div>
+              <div class="block">
+              <div class="thirdimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+                <img src="assets/img/instabul.jpg" alt="" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " height="250px" width="260px">
+                <h5 style="margin-bottom: 10px;margin-top: 20px;padding-bottom: 20px; padding-left: 20px;">Istanbul</h5>
+                <h6 style="margin-left: 20px;"> 11 sep 2022 - 22 sep 2022 <br>
+                  Economy from <br>
+                  USD 776 <br>
+                </h6>
+              </div> <br>
+              <div class="fourthimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+                <img src="assets/img/sydney.jpeg" alt="" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " width="260px"> <br> <br>
+                <h5 style="margin-top: 20px;padding-bottom: 30px; padding-left: 20px;">Sydney</h5>
+                <h6 style="margin-left: 20px;"> 11 sep 2022 - 22 sep 2022 <br>
+                  Economy from <br>
+                  USD 776 <br>
+                </h6>  
+              </div>
+            </div>
+          </div>
+        </div>
+      </section> <br><br>
+
+
+
+      <section>
+        <div class="start" style="width: 72%; margin-left: 200px;">
+         
+            <div class="flex d-flex gap-2">
+              <div class="thirdimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px; width:27.5%;">
+                <img src="assets/img/sanfrancisco.jpeg" width="100%" alt="" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " height="250px" width="260px">
+                <h5 style="margin-bottom: 10px;margin-top: 20px;padding-bottom: 20px; padding-left: 20px;">San Francisco, United States</h5>
+                <h6 style="margin-left: 20px;"> 11 sep 2022 - 22 sep 2022 <br>
+                  Economy from <br>
+                  USD 776 <br>
+                </h6>
+              </div> <br>
+              <div class="fourthimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px; width:27.5%;">
+                <img src="assets/img/santorini.jpeg" width="100%" alt="" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " height="250px" width="260px">
+                <h5 style="margin-top: 20px;padding-bottom: 30px; padding-left: 20px;">Santorini, Greece</h5>
+                <h6 style="margin-left: 20px;"> 11 sep 2022 - 22 sep 2022 <br>
+                  Economy from <br>
+                  USD 776 <br>
+                </h6>
+              </div>
+            
+              <div class="thirdimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px; width:27.5%;">
+                <img src="assets/img/seattle.jpeg" alt="" width="100%" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " height="250px" width="260px">
+                <h5 style="margin-bottom: 10px;margin-top: 20px;padding-bottom: 20px; padding-left: 20px;">Seattle, United States</h5>
+                <h6 style="margin-left: 20px;"> 11 sep 2022 - 22 sep 2022 <br>
+                  Economy from <br>
+                  USD 776 <br>
+                </h6>
+              </div> <br>
+              <div class="fourthimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px; width:27.5%;  ">
+                <img src="assets/img/sydney.jpeg" alt="" width="100%" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " height="250px" width="260px">
+                <h5 style="margin-top: 20px;padding-bottom: 30px; padding-left: 20px;">Sydney</h5>
+                <h6 style="margin-left: 20px;"> 11 sep 2022 - 22 sep 2022 <br>
+                  Economy from <br>
+                  USD 776 <br>
+                </h6>  
+              </div>
+          </div>
+        </div> 
+      </section>
+    </section> <br> <br>
+
+
+
+    <section>
+        <div class="start mx-auto pt-5" style="width: 70%;">
+          <div class="first-block d-flex" style="gap: 20px;">
+            <div class="firstimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px; border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+              <img src="assets/img/malaga.jpeg" alt="" height="520px" width="400px" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " height="620px" width="400px">
+              <h5 style="margin-bottom: 2px;margin-top: 50px;padding-bottom: -20px; padding-left: 20px;">Malaga, Spain</h5> <br>
+              <h6 style="margin-left: 20px;"> 18 july 2022 - 29 july 2022 <br>
+              Economy from <br>
+              USD 1,940 <br>
+            </h6>
+            </div>
+            <div class="block">
+              <div class="thirdimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+                <img src="assets/img/abidijan.jpeg" alt="" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " height="250px" width="260px">
+                <h5 style="margin-bottom: 10px;margin-top: 20px;padding-bottom: 20px; padding-left: 20px;">Abidijan, Ivory Coast</h5>
+                <h6 style="margin-left: 20px;"> 19 june 2022 - 24 june 2022 <br>
+                  Economy from <br>
+                  USD 2,100 <br>
+                </h6>
+              </div> <br>
+              <div class="fourthimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+                <img src="assets/img/dammam.jpeg" alt="" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " height="250px" width="260px">
+                <h5 style="margin-top: 20px;padding-bottom: 30px; padding-left: 20px;">Dammam Saudi, Arabia</h5>
+                <h6 style="margin-left: 20px;"> 16 june 2022 - 21 june 2022 <br>
+                  Economy from <br>
+                  USD 1,500 <br>
+                </h6>
+              </div>
+            </div>
+              <div class="block">
+              <div class="thirdimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+                <img src="assets/img/harare.jpeg" alt="" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " height="250px" width="260px">
+                <h5 style="margin-bottom: 10px;margin-top: 20px;padding-bottom: 20px; padding-left: 20px;">Harare, Zimbabwe</h5>
+                <h6 style="margin-left: 20px;"> 26 june 2022 - 01 july 2022 <br>
+                  Economy from <br>
+                  USD 500 <br>
+                </h6>
+              </div> <br>
+              <div class="fourthimg bg-white" style="border-top-left-radius: 20px; border-top-right-radius: 20px;border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+                <img src="assets/img/cebu.jpeg" alt="" style="border-top-left-radius: 20px; border-top-right-radius: 20px; " height="250px" width="260px">
+                <h5 style="margin-top: 20px;padding-bottom: 30px; padding-left: 20px;">Cebu, Phillipines</h5>
+                <h6 style="margin-left: 20px;"> 15 Aug 2022 - 10 sep 2022 <br>
+                  Economy from <br>
+                  USD 776 <br>
+                </h6>  
+              </div>
+            </div>
+          </div>
+        </div>
+        <br>
+        <button style="border: 1px solid purple; height: 40px; border-radius: 20px; background-color: rgba(151, 150, 150, 0.459); margin-left: 600px; margin-bottom: 40px;">
+          <a href="#" style="text-decoration: none; color:purple; margin-left: 20px; margin-right: 20px;">View more fares</a>
+        </button>
+      </section>
+    </div>
+
+    <script src="assets/js/jquery.3.6.0.js"></script>
+    <script src="assets/js/lightboxed.js"></script>
+</body>
+</html>
